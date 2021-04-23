255ef11db763da30e042adfdf1582847cbc82e11
wouter <wouter@valsecco.com>
2021-04-24 01:11:24 +0200

    change version number to 0.0.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 DESCRIPTION = "CRUD operations on nested SQLAlchemy ORM models using Pydantic"
 
 with open("README.md", "r") as file:
